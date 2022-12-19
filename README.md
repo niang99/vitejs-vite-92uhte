14be779fcbab24a30ae623d1d622d9ca48a3519a
niang99 <120993794+niang99@users.noreply.github.com>
2022-12-19 00:03:03 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-92uhte
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-92uhte)
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
